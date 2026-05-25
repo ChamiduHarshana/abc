35643237ced9b303d0aada38e1ca5716ad6cf3ed
chamidu <151662458+ChamiduHarshana@users.noreply.github.com>
2026-05-25 10:27:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '7e54366f3776c063' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'b135542f74a3099c' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
